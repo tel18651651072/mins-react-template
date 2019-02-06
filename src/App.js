3c5a34389110914c4a62be15287674716485ddf2
Apple <apple@AppledeMacBook-Pro.local>
2019-02-06 17:52:36 +0800

    修改index.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import './index.scss'
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div className="app">
+              我是home!!!!
+            </div>
+        )
+    }
+}
+
+export default App
